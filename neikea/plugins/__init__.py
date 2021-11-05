d925ad55b278d83dc34a7c534569226f6993f14e
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2021-11-04 20:20:22 -0700

    Add 'handler' and 'match' Processor decorators
    These decorators will be used to indicate what sort of messages/events a
    Processor function will handle.
    
    When `@handler` is used to decorate a function, it will handle all
    events that pass by it.
    The `@match` decorator is more specific: it indicates that an event's
    message has to match a regular expression before the function will try
    to handle it.
    
    We also gather all the functions with a True `handler` attribute in the
    Processor's constructor now, for use later.

--- neikea/plugins/__init__.py
+++ neikea/plugins/__init__.py
@@ -13,6 +13,29 @@ class Processor(object):
     def __init__(self, name):
         self.name = name
 
+        self._event_handlers = []
+        for n, item in type(self).__dict__.items():
+            if getattr(item, "handler", False):
+                self._event_handlers.append(item)
+
     async def process(self, event):
         "Process a single event"
         pass
+
+
+def handler(function):
+    "Wrapper: Handle all events"
+    function.handler = True
+    return function
+
+
+def match(regex):
+    "Wrapper: Handle all events where the message matches the regex"
+    pattern = re.compile(regex, re.I | re.UNICODE | re.DOTALL)
+
+    def wrap(function):
+        function.handler = True
+        function.pattern = pattern
+        return function
+
+    return wrap

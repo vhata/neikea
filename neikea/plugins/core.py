4d3c857b985680e0336993d62d491efc16e5966d
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2021-11-09 05:00:55 -0800

    Add 'banter' plugin which uses @handler
    This is a plugin that just advertises itself as a 'handler', because it
    does its own regex matching.
    
    It also adds a convenience function to interpolate variables like $day
    and $who in responses, to make them customized per event.
    
    Test Plan:
    
    ```
    $ DISCORD_TOKEN='XXXX' ./bot.py
    ```
    
    Then, on Discord:
    
    ```
    <Jonathan> hello
    <Spinach> howzit
    <Jonathan> botsnack
    <Spinach> :)
    <Jonathan> botsnack
    <Spinach> @Jonathan: thankyou!
    <Jonathan> interpolate dates
    <Spinach> year 2021 month2 11 month November mon Nov day2 11 day 11 date 2021-11-11 dow Thursday weekday Thursday
    <Jonathan> interpolate times
    <Spinach> hour 3 minute 48 second 10 time 3:48 PM unixtime 1636703290
    <Jonathan> interpolate discord
    <Spinach> who @Jonathan channel botdev server Wasteland
    ```

--- neikea/plugins/core.py
+++ neikea/plugins/core.py
@@ -1,10 +1,15 @@
-import datetime
+import logging
 import random
+import re
+from datetime import datetime
 
-from plugins import Processor, match
+from plugins import Processor, handler, match
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("neikea.plugins")
+
 
 time_replies = ["It's %H:%M!", "It's %-I:%M %p!"]
-
 date_replies = [
     "It's %A, %B %-d, %Y!",
     "According to my Gary Larson's Far Side desk calendar, it's %A, %B %-d, %Y!",
@@ -14,7 +19,7 @@ date_replies = [
 class Time(Processor):
     @match("time")
     async def time(self, event):
-        t = datetime.datetime.now()
+        t = datetime.now()
         await event.discord_message.channel.send(
             t.strftime(random.choice(time_replies))
         )
@@ -22,8 +27,154 @@ class Time(Processor):
 
     @match("date")
     async def date(self, event):
-        t = datetime.datetime.now()
+        t = datetime.now()
         await event.discord_message.channel.send(
             t.strftime(random.choice(date_replies))
         )
         event.processed = True
+
+
+greetings = (
+    "afternoon",
+    "bonjour",
+    "buon giorno",
+    "ello",
+    "evening",
+    "good day",
+    "good morning",
+    "hello",
+    "hey",
+    "heya",
+    "hi there",
+    "hi",
+    "hiya",
+    "hoe gaan dit",
+    "hoe lyk it",
+    "hoezit",
+    "hola",
+    "howdy",
+    "howsit",
+    "howzit",
+    "lo",
+    "llo",
+    "morning",
+    "salut",
+    "sup",
+    "wassup",
+    "wasup",
+    "what's up",
+    "word",
+    "wotcha",
+    "wotcher",
+    "wussup",
+    "yo",
+)
+banter_replies = {
+    "greet": {
+        "matches": [
+            r"\b("
+            + "|".join(
+                list(greetings) + [g.replace(" ", "") for g in greetings if " " in g]
+            )
+            + r")\b"
+        ],
+        "responses": greetings,
+    },
+    "reward": {
+        "matches": [r"\bbot(\s+|\-)?snack\b"],
+        "responses": ["thanks, $who", "$who: thankyou!", ":)"],
+    },
+    "praise": {
+        "matches": [
+            r"\bgood(\s+fuckin[\'g]?)?\s+(lad|bo(t|y)|g([ui]|r+)rl)\b",
+            r"\byou\s+(rock|rocks|rewl|rule|are\s+so+\s+co+l)\b",
+        ],
+        "responses": ["thanks, $who", "$who: thankyou!", ":)"],
+    },
+    "thanks": {
+        "matches": [r"\bthank(s|\s*you)\b", r"^\s*ta\s*$", r"^\s*shot\s*$"],
+        "responses": [
+            "no problem, $who",
+            "$who: my pleasure",
+            "sure thing, $who",
+            "no worries, $who",
+            "$who: np",
+            "no probs, $who",
+            "$who: no problemo",
+            "$who: not at all",
+        ],
+    },
+    "criticism": {
+        "matches": [
+            r"\b((kak|bad|st(u|oo)pid|dumb)(\s+fuckin[\'g]?)?\s+(bo(t|y)|g([ui]|r+)rl))|(bot(\s|\-)?s(mack|lap))\b",
+        ],
+        "responses": ["*whimper*", "sorry, $who :(", ":(", "*cringe*"],
+    },
+    "testdates": {
+        "matches": [
+            r"\binterpolate dates\b",
+        ],
+        "responses": [
+            "year $year month2 $month2 month $month mon $mon day2 $day2 day $day date $date dow $dow weekday $weekday",
+        ],
+    },
+    "testtimes": {
+        "matches": [
+            r"\binterpolate times\b",
+        ],
+        "responses": [
+            "hour $hour minute $minute second $second time $time unixtime $unixtime",
+        ],
+    },
+    "testdiscord": {
+        "matches": [
+            r"\binterpolate discord\b",
+        ],
+        "responses": ["who $who channel $channel server $server"],
+    },
+}
+
+
+def _interpolate(message, event):
+    "Expand factoid variables"
+    utcnow = datetime.utcnow()
+    now = datetime.now()
+
+    substitutions = [
+        ("who", f"<@!{event.discord_message.author.id}>"),
+        ("channel", f"<#{event.discord_message.channel.id}>"),
+        ("server", event.discord_message.guild.name),
+        ("year", str(now.year)),
+        ("month2", "%02i" % now.month),
+        ("month1", str(now.month)),
+        ("month", now.strftime("%B")),
+        ("mon", now.strftime("%b")),
+        ("day2", "%02i" % now.day),
+        ("day", str(now.day)),
+        ("hour", now.strftime("%-I")),
+        ("hour24", str(now.hour)),
+        ("minute", str(now.minute)),
+        ("second", str(now.second)),
+        ("date", now.strftime("%Y-%m-%d")),
+        ("time", now.strftime("%-I:%M %p")),
+        ("dow", now.strftime("%A")),
+        ("weekday", now.strftime("%A")),
+        ("unixtime", utcnow.strftime("%s")),
+    ]
+
+    for var, expansion in substitutions:
+        message = message.replace("$" + var, expansion)
+    return message
+
+
+class Banter(Processor):
+    @handler
+    async def static(self, event):
+        for banter in banter_replies.values():
+            for match in banter["matches"]:
+                if re.fullmatch(match, event.message, re.I):
+                    await event.discord_message.channel.send(
+                        _interpolate(random.choice(banter["responses"]), event)
+                    )
+                    event.processed = True
+                    return
